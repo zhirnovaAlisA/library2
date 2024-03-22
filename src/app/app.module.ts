60b6f4ab9a1f16785d646d9e14d4dbd3aee76255
zhirnovaAlisA <zirnovaalisa277@gmail.com>
2024-03-22 12:12:42 +0300

    1 ошибка не исправлена, добавила app.module

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import routeConfig from './app.routes'; // Импортируйте ваш routeConfig из app.routes.ts
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routeConfig)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 // import { appRoutes } from './app/app.routes';
 import routeConfig from './app/app.routes';
+import routes from './app/app.routes';
+import { RouterModule } from '@angular/router';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+
 
 bootstrapApplication(AppComponent,
   {
@@ -12,3 +17,21 @@ bootstrapApplication(AppComponent,
     ]
   }
 ).catch(err => console.error(err));
+
+
+// platformBrowserDynamic().bootstrapModule(AppModule, {
+//   imports: [RouterModule.forRoot(routes)], // Используем переменную routes
+// })
+// .catch(err => console.error(err));
+
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { AppModule } from './app/app.module'; 
+// import { RouterModule as AppRouterModule } from '@angular/router'; // Переименовываем импорт
+// import routeConfig from './app/app.routes'; 
+
+// platformBrowserDynamic().bootstrapModule(AppModule, [AppRouterModule.forRoot(routeConfig)])
+//   .catch(err => console.error(err));
+
+
+
+
