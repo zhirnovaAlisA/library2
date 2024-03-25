f42f5d6959a24adfdcc7602ac5bd718438a20b7d
zhirnovaAlisA <zirnovaalisa277@gmail.com>
2024-03-25 09:32:52 +0300

    работа над исправлением ошибки с router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 //абсолютно все компоненты и модули, которые присутствуют в проекте
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';
 import {AppRoutesModule} from './app.routes'; //добавлено
 import { RouterModule, RouterOutlet,   RouterLink, RouterLinkActive} from '@angular/router';
 import { BooksShowComponent } from './books-show/books-show.component';
@@ -9,16 +9,16 @@ import { BooksEditAddComponent } from './books-edit-add/books-edit-add.component
 
 @NgModule({
   declarations: [ //все компоненты
-    // AppComponent, 
+    AppComponent, 
   ],
   imports: [ //все модули
     BrowserModule,
     AppRoutesModule,
-    RouterModule, RouterOutlet, RouterLink, RouterLinkActive, BooksShowComponent, BooksEditAddComponent
+    // RouterModule, RouterOutlet, RouterLink, RouterLinkActive, BooksShowComponent, BooksEditAddComponent
     // RouterModule.forRoot(routeConfig), //добавлено, указание всех URL-адресов, которые мы отслеживаем(переменная со всеми урлами из рутс)
   ],
   providers: [],
-//   bootstrap: [AppComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
 
