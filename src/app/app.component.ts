0809d96e82ca2757b2fa5dcc6e0649fd532e3c8b
Anny777 <42442611+Anny777@users.noreply.github.com>
2024-03-26 10:39:57 +0300

    Починка роутинга (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet,   RouterLink, RouterLinkActive} from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { BooksShowComponent } from './books-show/books-show.component';
 
 
 @Component({
   selector: 'app-root', //селектор, который в index.html, мы отрисовываем именно этот компонент
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet]
 })
 
 
-export  class AppComponent { } 
+export class AppComponent { }  
