2641c0ca8a19588b8f461db9c8f964b8003adfcb
zhirnovaAlisA <zirnovaalisa277@gmail.com>
2024-03-27 10:28:24 +0300

    использование сервиса в компоненте

--- src/app/books-show/books-show.component.ts
+++ src/app/books-show/books-show.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Book } from '../models/book.model';
 import { CommonModule } from '@angular/common';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-books-show',
@@ -10,12 +11,31 @@ import { CommonModule } from '@angular/common';
   templateUrl: './books-show.component.html',
   styleUrl: './books-show.component.css'
 })
-export class BooksShowComponent {
-  // books: Book[] = [
-  //   { id: 1, title: 'Война и мир', author: 'Толстой', genre: 'Роман', cover_type: 'Мягкая', amount: 4, price: 799, purpose: 'Отдых'},
-  //   { id: 2, title: 'Мастер и Маргарита', author: 'Булгаков', genre: 'Роман', cover_type: 'Твёрдая', amount: 6, price: 1499, purpose: 'Образование'},
-  // ];
-  // deleteBook(id: number): void {
-  //   this.books = this.books.filter(book => book.id !== id);
-  // }
+// export class BooksShowComponent {
+//   books: Book[] = [
+//     { id: 1, title: 'Война и мир', author: 'Толстой', genre: 'Роман', cover_type: 'Мягкая', amount: 4, price: 799, purpose: 'Отдых'},
+//     { id: 2, title: 'Мастер и Маргарита', author: 'Булгаков', genre: 'Роман', cover_type: 'Твёрдая', amount: 6, price: 1499, purpose: 'Образование'},
+//   ];
+//   deleteBook(id: number): void {
+//     this.books = this.books.filter(book => book.id !== id);
+//   }
+// }
+export class BooksShowComponent implements OnInit {
+  books: Book[] = [];
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks(): void {
+    this.books = this.bookService.getBooks();
+  }
+  
+
+  deleteBook(id: number): void {
+    this.bookService.deleteBook(id); //метод удаления книги из сервиса
+    this.loadBooks(); // После удаления обновление списка книг в компоненте
+  }
 }
