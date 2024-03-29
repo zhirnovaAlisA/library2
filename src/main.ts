bee7166a7ea3c18b1471c29aa3e4ba298f52415d
Anny777 <42442611+Anny777@users.noreply.github.com>
2024-03-29 16:26:53 +0300

    Исправление конфигурации приложения (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
-// import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-// import { appRoutes } from './app/app.routes';
-// import routeConfig from './app/app.routes';
-// import routes from './app/app.routes';
-import { RouterModule } from '@angular/router';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
 
-
-
-// bootstrapApplication(AppComponent,
-//   {
-//     providers: [
-//       provideRouter(routeConfig)
-//     ]
-//   }
-// ).catch(err => console.error(err));
-
-
-// platformBrowserDynamic().bootstrapModule(AppModule, {
-//   imports: [RouterModule.forRoot(routes)], // Используем переменную routes
-// })
-// .catch(err => console.error(err));
-
-// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// import { AppModule } from './app/app.module'; 
-// import { RouterModule as AppRouterModule } from '@angular/router'; // Переименовываем импорт
-// import routeConfig from './app/app.routes'; 
-
-// platformBrowserDynamic().bootstrapModule(AppModule, [AppRouterModule.forRoot(routeConfig)])
-//   .catch(err => console.error(err));
-
+bootstrapApplication(AppComponent, config);
 
 
 
